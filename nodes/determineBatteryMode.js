0483692f4911d18accb24720e9d9808454cb5b59
iseeberg79 <iseeberg@gmx.de>
2025-01-13 00:01:42 +0100

    Debugnachrichten eingefügt

--- nodes/determineBatteryMode.js
+++ nodes/determineBatteryMode.js
@@ -16,7 +16,7 @@ module.exports = function(RED) {
 			// aus anderem Knoten berechnet
 			const enableGridcharge = msg.enableGridcharge = msg.enableGridcharge || false;
 			const optimize = msg.optimize = msg.optimize || false;
-			
+
 			// Standardwerte aus der Konfiguration übernehmen
 			const enableGridchargeThreshold = msg.enableGridchargeThreshold = (typeof msg.enableGridchargeThreshold !== 'undefined') ? msg.enableGridchargeThreshold : (node.enableGridchargeThreshold || 50);
 			const disableGridchargeThreshold = msg.disableGridchargeThreshold = (typeof msg.disableGridchargeThreshold !== 'undefined') ? msg.disableGridchargeThreshold : (node.disableGridchargeThreshold || 80);
@@ -36,7 +36,7 @@ module.exports = function(RED) {
 
 			// auch Ausgabewerte
 			let lastGridchargePrice = msg.lastGridchargePrice = (typeof msg.lastGridchargePrice !== 'undefined') ? msg.lastGridchargePrice : feedin;
-			
+
 			// Maximum zur Steuerung heranziehen: Glättung des Verbrauches
 			let batteryControlLimit = msg.batteryControlLimit = Math.max(lastGridchargePrice, avgPrice);
 			const loss = 1 + ((100 - efficiency) / 100);
@@ -56,68 +56,89 @@ module.exports = function(RED) {
 			}
 
 			// wenn Optimierung der Batterienutzung wirtschaftlich bzw. erlaubt
+
 			if (optimize) {
+				node.debug(`optimize is true`);
 				// Bewertung des Batteriestandes
 				let socControlMode;
 				if (soc > disableGridchargeThreshold) {
+					node.debug(`soc (${soc}) > disableGridchargeThreshold (${disableGridchargeThreshold})`);
 					socControlMode = "highSOC";
 				} else if (soc <= enableGridchargeThreshold) {
+					node.debug(`soc (${soc}) <= enableGridchargeThreshold (${enableGridchargeThreshold})`);
 					socControlMode = "lowSOC";
 				} else {
+					node.debug(`soc (${soc}) is between enableGridchargeThreshold (${enableGridchargeThreshold}) and disableGridchargeThreshold (${disableGridchargeThreshold})`);
 					socControlMode = "mediumSOC";
 				}
 
 				// Zurücksetzen des letzten Ladepreises bei geringem/hohem Füllstand
 				switch (socControlMode) {
 					case "highSOC":
+						node.debug(`socControlMode is highSOC`);
 						if (soc > resetmaxsoc) {
+							node.debug(`soc (${soc}) > resetmaxsoc (${resetmaxsoc})`);
 							lastGridchargePrice = feedin * loss;
 						}
 						break;
 					case "lowSOC":
+						node.debug(`socControlMode is lowSOC`);
 						if (soc < resetminsoc) {
+							node.debug(`soc (${soc}) < resetminsoc (${resetminsoc})`);
 							lastGridchargePrice = feedin * loss;
 						}
 						break;
 					case "mediumSOC":
+						node.debug(`socControlMode is mediumSOC`);
 						break;
 				}
 
 				// Wintermonate?
 				let winterMode = isWinter((new Date()).getMonth());
+				node.debug(`winterMode is ${winterMode}`);
 
 				// Bestimmung von pvControlMode
 				let pvControlMode;
 				if (winterMode) {
 					msg.estimatedConsumption = (batteryCapacity - (msg.soc / 100 * batteryCapacity) + estimatedHousehold);
+					node.debug(`estimatedConsumption is ${msg.estimatedConsumption}`);
 					pvControlMode = (pvforecast < msg.estimatedConsumption) ? "insufficientPV" : "sufficientPV";
+					node.debug(`pvControlMode is ${pvControlMode}`);
 				} else {
 					pvControlMode = "sufficientPV";
+					node.debug(`pvControlMode is sufficientPV`);
 				}
 
 				// Logik für Netzladung bei günstigem Strompreis
 				if (pvControlMode === "insufficientPV" && pvControlMode !== "highSOC") {
+					node.debug(`pvControlMode is insufficientPV and not highSOC`);
 					msg.targetMode = "hold";
 					// Netzladung erlauben?
 					if ((socControlMode === "lowSOC") || (socControlMode === "mediumSOC") && (msg.batterymode === "charge")) {
+						node.debug(`socControlMode is ${socControlMode} and batterymode is charge`);
 						let minTotal = minPrice ? parseFloat((minPrice * 1.02).toFixed(3)) : 0;
 						if (enableGridcharge && mayChargeBattery(price, minTotal, msg.avgGridPriceWeekly)) {
+							node.debug(`enableGridcharge is true and mayChargeBattery returns true`);
 							msg.targetMode = "charge";
 							let gridchargePrice = (price / 100 * loss);
 							if (gridchargePrice > lastGridchargePrice) {
+								node.debug(`gridchargePrice (${gridchargePrice}) > lastGridchargePrice (${lastGridchargePrice})`);
 								lastGridchargePrice = gridchargePrice;
 							}
 						}
 					}
-					// Batterie darf entladen, wenn Strompreis hoch	und Batterie nicht teuer geladen wurde
+					// Batterie darf entladen, wenn Strompreis hoch und Batterie nicht teuer geladen wurde
 					if (price > batteryControlLimit && price > lastGridchargePrice) {
+						node.debug(`price (${price}) > batteryControlLimit (${batteryControlLimit}) and price > lastGridchargePrice (${lastGridchargePrice})`);
 						msg.targetMode = "normal";
 					}
 				} else {
 					// Batterie darf entladen, wenn PV Leistung des Tages ausreicht
+					node.debug(`pvControlMode is sufficientPV or highSOC`);
 					msg.targetMode = "normal";
 				}
 			} else {
+				node.debug(`optimize is false`);
 				// Batterie darf entladen, wenn Opti
 				msg.targetMode = "normal";
 			}
--- nodes/determineControlMode.js
+++ nodes/determineControlMode.js
@@ -1,81 +1,94 @@
 module.exports = function(RED) {
-    function DetermineControlModeNode(config) {
-        RED.nodes.createNode(this, config);
-        const node = this;
-        node.minPvRequired = config.minPvRequired;
-        node.minPriceDeviation = config.minPriceDeviation;
-        node.minPriceDifference = config.minPriceDifference;
-        node.priceLimit = config.priceLimit;
-        node.avg = config.avg;
+	function DetermineControlModeNode(config) {
+		RED.nodes.createNode(this, config);
+		const node = this;
+		node.minPvRequired = config.minPvRequired;
+		node.minPriceDeviation = config.minPriceDeviation;
+		node.minPriceDifference = config.minPriceDifference;
+		node.priceLimit = config.priceLimit;
+		node.avg = config.avg;
 
-        node.on('input', function(msg) {
-            // Konfiguration
-            const minPvRequired = (typeof msg.minPvRequired !== 'undefined') ? msg.minPvRequired : (node.minPvRequired || 10000);
-            const minPriceDeviation = (typeof msg.minPriceDeviation !== 'undefined') ? msg.minPriceDeviation : (node.minPriceDeviation || 6);
-            const minPriceDifference = (typeof msg.minPriceDifference !== 'undefined') ? msg.minPriceDifference : (node.minPriceDifference || 15);
-            const priceLimit = ((typeof msg.priceLimit !== 'undefined') ? msg.priceLimit : (node.priceLimit || node.avg || 0.25)) * 100;
+		node.on('input', function(msg) {
+			// Konfiguration
+			const minPvRequired = (typeof msg.minPvRequired !== 'undefined') ? msg.minPvRequired : (node.minPvRequired || 10000);
+			const minPriceDeviation = (typeof msg.minPriceDeviation !== 'undefined') ? msg.minPriceDeviation : (node.minPriceDeviation || 6);
+			const minPriceDifference = (typeof msg.minPriceDifference !== 'undefined') ? msg.minPriceDifference : (node.minPriceDifference || 15);
+			const priceLimit = ((typeof msg.priceLimit !== 'undefined') ? msg.priceLimit : (node.priceLimit || node.avg || 0.25)) * 100;
 
-            // Werte zur Berechnung, mit sicheren Standards belegt
-            const avgPriceWeekly = ((typeof msg.avgWeekly !== 'undefined') ? msg.avgWeekly : (node.avg || priceLimit)) * 100;
-            const avgPrice = ((typeof msg.average !== 'undefined') ? msg.average : node.avg || 0.25) * 100;
-            const pvForecast = (typeof msg.pvforecast !== 'undefined') ? msg.pvforecast : (node.pvforecast || minPvRequired);
-            const priceDeviation = (typeof msg.deviation !== 'undefined') ? msg.deviation : (node.deviation || 0);
-            const priceDifference = (typeof msg.diff !== 'undefined') ? msg.diff : (node.diff || 0);
+			// Werte zur Berechnung, mit sicheren Standards belegt
+			const avgPriceWeekly = ((typeof msg.avgWeekly !== 'undefined') ? msg.avgWeekly : (node.avg || priceLimit)) * 100;
+			const avgPrice = ((typeof msg.average !== 'undefined') ? msg.average : node.avg || 0.25) * 100;
+			const pvForecast = (typeof msg.pvforecast !== 'undefined') ? msg.pvforecast : (node.pvforecast || minPvRequired);
+			const priceDeviation = (typeof msg.deviation !== 'undefined') ? msg.deviation : (node.deviation || 0);
+			const priceDifference = (typeof msg.diff !== 'undefined') ? msg.diff : (node.diff || 0);
 
-            // Maximum zur Steuerung heranziehen: Glättung des Verbrauches
-            const batteryControlLimit = msg.batteryControlLimit = Math.max(priceLimit, avgPriceWeekly);
+			// Maximum zur Steuerung heranziehen: Glättung des Verbrauches
+			const batteryControlLimit = msg.batteryControlLimit = Math.max(priceLimit, avgPriceWeekly);
 
-            // Logik zur Bestimmung der Steuerung
-            if (pvForecast < minPvRequired) {
-                 msg.pvMIN = true;
-                if (Math.round(priceDifference*10) > Math.round(minPriceDifference*10)) {
-                     msg.priceOK = true; msg.priceDIFFERENCE = minPriceDifference;
-                    if (pvForecast * 1.6 < minPvRequired) {
-                        msg.payload = { optimize: true, gridcharge: true, mode: 'GRID' };
-                    } else {
-                        msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
-                    }
-                } else {
-                    if (Math.round(priceDeviation*10) > Math.round(minPriceDeviation*10)) {
-                        msg.priceFallback = true; msg.priceDEVIATION = priceDeviation;
-                        msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
-                    } else {
-                        if (Math.round(avgPrice*100) > Math.round(batteryControlLimit*100)) {
-                            msg.averageHIGH = true;
-                            msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
-                        } else {
-                            msg.averageLOW = true;
-                            msg.payload = { optimize: false, gridcharge: false, mode: 'evcc' };
-                        }
-                    }
-                }
-            } else {
-                msg.pvOK = true; msg.pvFORECAST = pvForecast;
-                msg.payload = { optimize: false, gridcharge: false, mode: 'evcc' };
-            }
+			// Logik zur Bestimmung der Steuerung
+			if (pvForecast < minPvRequired) {
+				node.debug(`pvForecast (${pvForecast}) < minPvRequired (${minPvRequired})`);
+				msg.pvMIN = true;
+				if (Math.round(priceDifference * 10) > Math.round(minPriceDifference * 10)) {
+					node.debug(`priceDifference (${priceDifference}) > minPriceDifference (${minPriceDifference})`);
+					msg.priceOK = true;
+					msg.priceDIFFERENCE = minPriceDifference;
+					if (pvForecast * 1.6 < minPvRequired) {
+						node.debug(`pvForecast * 1.6 (${pvForecast * 1.6}) < minPvRequired (${minPvRequired})`);
+						msg.payload = { optimize: true, gridcharge: true, mode: 'GRID' };
+					} else {
+						node.debug(`pvForecast * 1.6 (${pvForecast * 1.6}) >= minPvRequired (${minPvRequired})`);
+						msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
+					}
+				} else {
+					node.debug(`priceDifference (${priceDifference}) <= minPriceDifference (${minPriceDifference})`);
+					if (Math.round(priceDeviation * 10) > Math.round(minPriceDeviation * 10)) {
+						node.debug(`priceDeviation (${priceDeviation}) > minPriceDeviation (${minPriceDeviation})`);
+						msg.priceFallback = true;
+						msg.priceDEVIATION = priceDeviation;
+						msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
+					} else {
+						node.debug(`priceDeviation (${priceDeviation}) <= minPriceDeviation (${minPriceDeviation})`);
+						if (Math.round(avgPrice * 100) > Math.round(batteryControlLimit * 100)) {
+							node.debug(`avgPrice (${avgPrice}) > batteryControlLimit (${batteryControlLimit})`);
+							msg.averageHIGH = true;
+							msg.payload = { optimize: true, gridcharge: false, mode: 'LIMIT' };
+						} else {
+							node.debug(`avgPrice (${avgPrice}) <= batteryControlLimit (${batteryControlLimit})`);
+							msg.averageLOW = true;
+							msg.payload = { optimize: false, gridcharge: false, mode: 'evcc' };
+						}
+					}
+				}
+			} else {
+				node.debug(`pvForecast (${pvForecast}) >= minPvRequired (${minPvRequired})`);
+				msg.pvOK = true;
+				msg.pvFORECAST = pvForecast;
+				msg.payload = { optimize: false, gridcharge: false, mode: 'evcc' };
+			}
 
-            if (msg.payload.gridcharge) {
-                node.status({ fill: "orange", shape: "dot", text: msg.payload.mode });
-            } else {
-                if (msg.payload.optimize) {
-                    node.status({ fill: "yellow", shape: "dot", text: msg.payload.mode });
-                } else {
-                    node.status({ fill: "green", shape: "dot", text: msg.payload.mode });
-                }
-            }
+			if (msg.payload.gridcharge) {
+				node.status({ fill: "orange", shape: "dot", text: msg.payload.mode });
+			} else {
+				if (msg.payload.optimize) {
+					node.status({ fill: "yellow", shape: "dot", text: msg.payload.mode });
+				} else {
+					node.status({ fill: "green", shape: "dot", text: msg.payload.mode });
+				}
+			}
 
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType('@iseeberg79/DetermineControlMode', DetermineControlModeNode, {
-        defaults: {
-            name: { value: "" },
-            minPvRequired: { value: 16000 },
-            minPriceDeviation: { value: 6 },
-            minPriceDifference: { value: 15 },
-            priceLimit: { value: 0.25 },
-            avg: { value: 0.25 }
-        }
-    });
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType('@iseeberg79/DetermineControlMode', DetermineControlModeNode, {
+		defaults: {
+			name: { value: "" },
+			minPvRequired: { value: 16000 },
+			minPriceDeviation: { value: 6 },
+			minPriceDifference: { value: 15 },
+			priceLimit: { value: 0.25 },
+			avg: { value: 0.25 }
+		}
+	});
 }
 
