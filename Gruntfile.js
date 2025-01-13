f6d9b699f9c5ad32c81c5e6887aad918fec92fa2
iseeberg79 <iseeberg@gmx.de>
2025-01-13 22:01:44 +0100

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,38 @@
 module.exports = function(grunt) {
-
-	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-//		jshint: {
-//			src: ['nodes/*.js'],
-//			options: { esversion: 6, curly: true, eqeqeq: true, undef: true, unused: true, globals: { jQuery: true } }
-//		},
-//		jsdoc: {
-//			options: {
-//				destination: 'build/docs'
-//			},
-//			dist: {
-//				src: ['nodes/*.js']
-//			}
-//		},
-		uglify: {
+		jshint: {
+			src: ['nodes/*.js'],
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build: {
-				src: 'nodes/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+				esversion: 6,
+				curly: true,
+				eqeqeq: true,
+				undef: true,
+				unused: true,
+				globals: {
+					jQuery: true
+				}
+			}
+		},
+		jsdoc: {
+			dist: {
+				src: ['nodes/*.js'],
+				options: {
+					destination: 'build/docs'
+				}
+			}
+		},
+		shell: {
+			npm_pack: {
+				command: 'npm pack --pack-destination ./build'
 			}
 		}
 	});
 
-//	grunt.loadNpmTasks('grunt-contrib-jshint');
-//	grunt.loadNpmTasks('grunt-jsdoc');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-shell');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
-	//grunt.registerTask('default', ['jshint', 'jsdoc', 'uglify']);
-
+	grunt.registerTask('default', ['jshint', 'jsdoc', 'shell:npm_pack']);
 };
