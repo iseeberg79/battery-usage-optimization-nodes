c647e6de3c5ec2ba36ebf09f906a0de6b357aae8
iseeberg79 <iseeberg@gmx.de>
2025-02-24 02:15:48 +0100

    recent data instead of now and formatting

--- nodes/evaluateSolarForecastAPI.js
+++ nodes/evaluateSolarForecastAPI.js
@@ -1,21 +1,21 @@
-const axios = require('axios');
+const axios = require("axios");
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function EvaluateSolarForecastAPI(config) {
         RED.nodes.createNode(this, config);
         const node = this;
-        node.url = config.url || 'http://localhost:7070/api/tariff/solar';
+        node.url = config.url || "http://localhost:7070/api/tariff/solar";
 
-        node.on('input', async function(msg) {
+        node.on("input", async function (msg) {
             // Defaults to provided URL or local API
-            msg.url = (typeof msg.url !== 'undefined') ? msg.url : node.url;
+            msg.url = typeof msg.url !== "undefined" ? msg.url : node.url;
 
             // HTTP request
             try {
                 const response = await axios.get(msg.url);
                 msg.response = response.data;
             } catch (error) {
-                node.error('HTTP-Anfrage Fehler: ' + error, msg);
+                node.error("HTTP-Anfrage Fehler: " + error, msg);
                 return;
             }
 
@@ -23,14 +23,18 @@ module.exports = function(RED) {
             msg.payload = msg.response;
 
             delete msg.response;
-            
+
             node.send(msg);
         });
     }
-    RED.nodes.registerType('EvaluateSolarForecastAPI', EvaluateSolarForecastAPI, {
-        defaults: {
-            name: { value: "" },
-            url: { value: "http://localhost:7070/api/tariff/solar" }
-        }
-    });
+    RED.nodes.registerType(
+        "EvaluateSolarForecastAPI",
+        EvaluateSolarForecastAPI,
+        {
+            defaults: {
+                name: { value: "" },
+                url: { value: "http://localhost:7070/api/tariff/solar" },
+            },
+        },
+    );
 };
