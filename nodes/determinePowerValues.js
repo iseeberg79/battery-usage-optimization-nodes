b13bc65332fc20428fb0cbc80039fbe5aa58eafe
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-02-24 02:05:33 +0100

    corrections on estimation (#26)
    * corrections on soc/energy and estimation
    
    * solarForecast by evcc-API (initial)

--- nodes/determinePowerValues.js
+++ nodes/determinePowerValues.js
@@ -1,60 +1,76 @@
-const axios = require('axios');
-module.exports = function(RED) {
-	function DeterminePowerValues(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
-		node.url = config.url;
+const axios = require("axios");
+module.exports = function (RED) {
+    function DeterminePowerValues(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.url = config.url;
 
-		node.on('input', async function(msg) {
-			msg.url = (typeof msg.url !== 'undefined') ? msg.url : (node.url || 'http://localhost:7070/api/state');
+        node.on("input", async function (msg) {
+            msg.url =
+                typeof msg.url !== "undefined"
+                    ? msg.url
+                    : node.url || "http://localhost:7070/api/state";
 
-			try {
-				const response = await axios.get(msg.url);
-				msg.response = response.data;
-			} catch (error) {
-				node.error('HTTP-Anfrage Fehler: ' + error, msg);
-				return;
-			}
+            try {
+                const response = await axios.get(msg.url);
+                msg.response = response.data;
+            } catch (error) {
+                node.error("HTTP-Anfrage Fehler: " + error, msg);
+                return;
+            }
 
-			// Überprüfe, ob die Nachricht die alte oder neue API-Struktur (01/2025) von evcc hat
-			let gridPower = msg.response.result.gridPower || msg.response.result.grid.power || 0;
-					
-			let homePower = msg.response.result.homePower || 0;
-			let pvPower = msg.response.result.pv.power || 0;
+            // Überprüfe, ob msg.response.result existiert und sinnvoll gefüllt ist
+            const result = msg.response.result;
+            if (!result) {
+                node.error(
+                    "Ungültige Antwortstruktur: result ist nicht definiert oder leer",
+                    msg,
+                );
+                return;
+            }
 
-			let batteryPower = msg.response.result.batteryPower || 0;
-			let batterySoc = msg.response.result.batterySoc || 0;
-									
-			let tariffGrid = msg.response.result.tariffGrid || 0.00;
-			let batteryMode = msg.response.result.batteryMode || "unknown";
-			let interval = msg.response.result.interval || 60;
-			
-			const sumPower = msg.response.result.loadpoints.reduce((sum, lp) => sum + lp.chargePower, 0);
+            let gridPower =
+                result.gridPower || (result.grid && result.grid.power) || 0;
+            let homePower = result.homePower || 0;
+            let pvPower = (result.pv && result.pv.power) || 0;
+            let batteryPower = result.batteryPower || 0;
+            let batterySoc = result.batterySoc || 0;
+            let tariffGrid = result.tariffGrid || 0.0;
+            let batteryMode = result.batteryMode || "unknown";
+            let interval = result.interval || 60;
 
-			msg.payload = {
-				homePower: homePower,
-				loadpointsPower: sumPower,
-				gridPower: gridPower,
-				tariffGrid: tariffGrid,
-				batteryPower: batteryPower,
-				pvPower: pvPower,
-				batterySoc: batterySoc,
-				batteryMode: batteryMode,
-				interval: interval,
-				lastchange: (new Date()).getTime()
-			};
+            const sumPower = result.loadpoints
+                ? result.loadpoints.reduce((sum, lp) => sum + lp.chargePower, 0)
+                : 0;
 
-			delete msg.response;
+            msg.payload = {
+                homePower: homePower,
+                loadpointsPower: sumPower,
+                gridPower: gridPower,
+                tariffGrid: tariffGrid,
+                batteryPower: batteryPower,
+                pvPower: pvPower,
+                batterySoc: batterySoc,
+                batteryMode: batteryMode,
+                interval: interval,
+                lastchange: new Date().getTime(),
+            };
 
-			node.send(msg);
-		});
-	}
-	RED.nodes.registerType('@iseeberg79/DeterminePowerValues', DeterminePowerValues, {
-		defaults: {
-			name: { value: "" },
-			url: { value: "http://localhost:7070/api/state" }
-		},
-		inputs: 1,
-		outputs: 1
-	});
+            delete msg.response;
+
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType(
+        "@iseeberg79/DeterminePowerValues",
+        DeterminePowerValues,
+        {
+            defaults: {
+                name: { value: "" },
+                url: { value: "http://localhost:7070/api/state" },
+            },
+            inputs: 1,
+            outputs: 1,
+        },
+    );
 };
---
+++ nodes/evaluateSolarForecastAPI.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+module.exports = function(RED) {
+    function EvaluateSolarForecastAPI(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        node.url = config.url || 'http://localhost:7070/api/tariff/solar';
+
+        node.on('input', async function(msg) {
+            // Defaults to provided URL or local API
+            msg.url = (typeof msg.url !== 'undefined') ? msg.url : node.url;
+
+            // HTTP request
+            try {
+                const response = await axios.get(msg.url);
+                msg.response = response.data;
+            } catch (error) {
+                node.error('HTTP-Anfrage Fehler: ' + error, msg);
+                return;
+            }
+
+            // Forward the JSON response as payload
+            msg.payload = msg.response;
+
+            delete msg.response;
+            
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType('EvaluateSolarForecastAPI', EvaluateSolarForecastAPI, {
+        defaults: {
+            name: { value: "" },
+            url: { value: "http://localhost:7070/api/tariff/solar" }
+        }
+    });
+};
