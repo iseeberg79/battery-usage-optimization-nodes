0ed162b171aa167ea96ab561080aa717e0d5dc75
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-03-08 00:38:59 +0100

    version 0.1.5 (#42)
    * Update README.md (#40) (#41)
    
    * updated examples
    
    * adapted roundings
    
    * ISO formatting
    
    * pretty print
    
    * version 0.1.5

--- nodes/evaluateSolarForecastAPI.js
+++ nodes/evaluateSolarForecastAPI.js
@@ -1,83 +1,96 @@
 const axios = require("axios");
 
-module.exports = function (RED) {
-    function EvaluateSolarForecastAPINode(config) {
-        RED.nodes.createNode(this, config);
-        const node = this;
-        node.url = config.url || "http://localhost:7070/api/tariff/solar";
+module.exports = function(RED) {
+	function EvaluateSolarForecastAPINode(config) {
+		RED.nodes.createNode(this, config);
+		const node = this;
+		node.url = config.url || "http://localhost:7070/api/tariff/solar";
 
-        node.on("input", async function (msg) {
-            // Defaults to provided URL or local API
-            msg.url = typeof msg.url !== "undefined" ? msg.url : node.url;
+		node.on("input", async function(msg) {
+			// Defaults to provided URL or local API
+			msg.url = typeof msg.url !== "undefined" ? msg.url : node.url;
 
-            // HTTP request
-            try {
-                const response = await axios.get(msg.url);
-                msg.payload = response.data;
-            } catch (error) {
-                node.error("HTTP-Anfrage Fehler: " + error, msg);
-                return;
-            }
+			// HTTP request
+			try {
+				const response = await axios.get(msg.url);
+				msg.payload = response.data;
+			} catch (error) {
+				node.error("HTTP-Anfrage Fehler: " + error, msg);
+				return;
+			}
 
-            // Umbenennen von 'price' in 'value' und Umrechnung in kWh
-            const rates = msg.payload.result.rates.map((item) => {
-                return {
-                    ...item,
-                    value: item.price / 1000, // Umrechnung von Wh in kWh
-                };
-            });
+			// Umbenennen von 'price' in 'value' und Umrechnung in kWh
+			const rates = msg.payload.result.rates.map((item) => {
+				return {
+					...item,
+					value: item.price / 1000, // Umrechnung von Wh in kWh
+				};
+			});
 
-            // Aktuelles Datum und Zeit
-            const now = new Date();
+			// Aktuelles Datum und Zeit
+			const now = new Date();
 
-            // Funktion, um die Erträge für einen bestimmten Tag zu berechnen
-            function calculateTotalForDay(day) {
-                return rates.reduce((sum, item) => {
-                    const startDate = new Date(item.start);
-                    if (startDate.toISOString().split("T")[0] === day) {
-                        return sum + item.value;
-                    }
-                    return sum;
-                }, 0);
-            }
+			// Funktion, um die Erträge für einen bestimmten Tag zu berechnen
+			function calculateTotalForDay(day) {
+				return rates.reduce((sum, item) => {
+					const startDate = new Date(item.start);
+					if (startDate.toISOString().split("T")[0] === day) {
+						return sum + item.value;
+					}
+					return sum;
+				}, 0);
+			}
 
-            // Berechnung der heutigen und morgigen Erträge
-            const today = now.toISOString().split("T")[0];
-            const tomorrow = new Date(now);
-            tomorrow.setDate(tomorrow.getDate() + 1);
-            const tomorrowDateString = tomorrow.toISOString().split("T")[0];
+			// Berechnung der heutigen und morgigen Erträge
+			const today = now.toISOString().split("T")[0];
+			const tomorrow = new Date(now);
+			tomorrow.setDate(tomorrow.getDate() + 1);
+			const tomorrowDateString = tomorrow.toISOString().split("T")[0];
 
-            const todayTotal = calculateTotalForDay(today) * 1000;
-            const tomorrowTotal = calculateTotalForDay(tomorrowDateString) * 1000;
+			const todayTotal = calculateTotalForDay(today) * 1000;
+			const tomorrowTotal = calculateTotalForDay(tomorrowDateString) * 1000;
 
-            // Berechnung der verbleibenden Erträge für heute
-            const remainderToday =
-                rates.reduce((sum, item) => {
-                    const startDate = new Date(item.start);
-                    if (startDate > now && startDate.toISOString().split("T")[0] === today) {
-                        return sum + item.value;
-                    }
-                    return sum;
-                }, 0) * 1000;
+			// Berechnung der verbleibenden Erträge für heute
+			const remainderToday =
+				rates.reduce((sum, item) => {
+					const startDate = new Date(item.start);
+					if (startDate > now && startDate.toISOString().split("T")[0] === today) {
+						return sum + item.value;
+					}
+					return sum;
+				}, 0) * 1000;
 
-            // Forward the JSON response as payload
-            msg.payload.rates = rates;
+			// Funktion zur Formatierung des Zeitstempels nach ISO
+			const formatTimestamp = (timestamp) => {
+				const date = new Date(timestamp);
+				return date.toISOString().replace(/\.\d{1,7}Z$/, 'Z');
+			};
 
-            msg.payload.today = Math.floor(todayTotal * 100) / 100;
-            msg.payload.tomorrow = Math.floor(tomorrowTotal * 100) / 100;
-            msg.payload.remain = Math.floor(remainderToday * 100) / 100;
-            msg.payload.lastchange = new Date().getTime();
+			// Array durchlaufen und Zeitstempel formatieren
+			const formattedData = rates.map(item => ({
+				start: formatTimestamp(item.start),
+				end: formatTimestamp(item.end),
+				price: item.price
+			}));
 
-            // cleanup
-            delete msg.payload.result;
+			// Forward the JSON response as payload
+			msg.payload.rates = formattedData;
 
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("@iseeberg79/EvaluateSolarForecastAPI", EvaluateSolarForecastAPINode, {
-        defaults: {
-            name: { value: "" },
-            url: { value: "http://localhost:7070/api/tariff/solar" },
-        },
-    });
+			msg.payload.today = Math.floor(todayTotal * 100) / 100;
+			msg.payload.tomorrow = Math.floor(tomorrowTotal * 100) / 100;
+			msg.payload.remain = Math.floor(remainderToday * 100) / 100;
+			msg.payload.lastchange = new Date().getTime();
+
+			// cleanup
+			delete msg.payload.result;
+
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType("@iseeberg79/EvaluateSolarForecastAPI", EvaluateSolarForecastAPINode, {
+		defaults: {
+			name: { value: "" },
+			url: { value: "http://localhost:7070/api/tariff/solar" },
+		},
+	});
 };
